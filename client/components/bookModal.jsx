88d0f910a5bc09d5b94cec1c7f8db6e58e229dd8
Jesús Ramírez <jesusmarcanody@gmail.com>
2022-02-16 15:40:01 -0400

    Update bookModal.jsx
    feat: redesign book modal

--- client/components/bookModal.jsx
+++ client/components/bookModal.jsx
@@ -1,14 +1,13 @@
-import { Box, Heading, Text, Modal, ModalOverlay, ModalContent, HStack, IconButton, Icon, Divider, Img, Grid, GridItem, Spacer, Button, Stat, StatLabel, StatNumber } from '@chakra-ui/react'
+import { Box, Heading, Text, Modal, ModalOverlay, ModalContent, HStack, IconButton, Icon, Divider, Img, Grid, GridItem, Spacer, Button, Stat, StatLabel, StatNumber, ModalCloseButton, Tag, TagLeftIcon, TagLabel, Badge, Wrap } from '@chakra-ui/react'
 import { GoHeart, GoX } from 'react-icons/go'
 import { useRouter} from "next/router"
-import { IoBookmark, IoBookmarkOutline, IoClose, IoEllipsisVertical, IoHeart, IoHeartOutline } from 'react-icons/io5'
+import { IoArrowForward, IoBookmark, IoBookmarkOutline, IoCalendar, IoClose, IoEllipsisVertical, IoHeart, IoHeartOutline, IoImage } from 'react-icons/io5'
 import useTimeago from 'hooks/useTimeago'
+import Link from 'next/link'
 
 const modalStyles = {
   bg: 'white',
   rounded: 16,
-  border: '1px',
-  borderColor: 'gray.100',
   w: 'full',
   h: 'full',
   templateRows: 'repeat(2, 1fr)',
@@ -22,7 +21,7 @@ const topActionButtonStyle = {
 
 export default function BookModal({book, isOpen}) {
   const router = useRouter()
-  const { title, medium, createdAt } = book
+  const { title, medium, createdAt, bookUrl, nsfw } = book
   const handleClose = () => {
     router.back()
   }
@@ -35,51 +34,72 @@ export default function BookModal({book, isOpen}) {
       <ModalOverlay />
       <ModalContent bg="none" shadow="none">
         <Box {...modalStyles}>
-          <HStack p={2}>
-            <IconButton
-              {...topActionButtonStyle}
-              icon={<Icon as={IoClose} />}
-              onClick={handleClose}
-              size="sm"
+          <Box
+            height="lg"
+            bgImage={medium}
+            bgSize="cover"
+            bgPosition="center"
+            rounded={16}  
+            >
+            <Img
+              src={medium}
+              w="full"
+              objectFit="contain"
+              backdropFilter="blur(25px)"
+              height="lg"
+              rounded={16}  
+              p={4}
             />
-            <Text
-              isTruncated
-              fontSize="sm"
-              fontWeight="semibold"
-            >{title}</Text>
-            <Spacer />
-            <IconButton
-              {...topActionButtonStyle}
-              icon={<Icon as={IoBookmarkOutline} />}
-            />
-            <IconButton
-              {...topActionButtonStyle}
-              icon={<Icon as={IoHeartOutline} />}
-            />
-            <IconButton
-              {...topActionButtonStyle}
-              icon={<Icon as={IoEllipsisVertical} />}
-            />
-          </HStack>
-          <Divider />
-          <Img
-            src={medium}
-            w="full"
-            objectFit="contain"
-            p={4}
-          />
-          <HStack p={4}>
-            <Stat>
-              <StatLabel>Images</StatLabel>
-              <StatNumber>32</StatNumber>
-            </Stat>
-            <Stat>
-              <StatLabel>Published</StatLabel>
-              <StatNumber>{useTimeago(new Date(createdAt))}</StatNumber>
-            </Stat>
-            <Spacer />
-            <Button colorScheme="messenger" rounded="full">View book</Button>
-          </HStack>
+            <ModalCloseButton />
+          </Box>
+          <Box p={8}>
+            <HStack>
+              <Text
+                isTruncated
+                fontSize="sm"
+                fontWeight="semibold"
+              >{title}</Text>
+              <Spacer />
+              <IconButton
+                {...topActionButtonStyle}
+                icon={<Icon as={IoBookmarkOutline} />}
+              />
+              <IconButton
+                {...topActionButtonStyle}
+                icon={<Icon as={IoHeartOutline} />}
+              />
+              <IconButton
+                {...topActionButtonStyle}
+                icon={<Icon as={IoEllipsisVertical} />}
+              />
+            </HStack>
+            <HStack mt={4}>
+              <Wrap>
+                <Tag size="lg">
+                  <TagLeftIcon as={IoCalendar} />
+                  <TagLabel>{useTimeago(new Date(createdAt))}</TagLabel>
+                </Tag>
+                <Tag size="lg">
+                  <TagLeftIcon as={IoImage} />
+                  <TagLabel>32</TagLabel>
+                </Tag>
+                {
+                  nsfw &&
+                  <Tag size="lg" colorScheme="red">NSFW</Tag>
+                }
+              </Wrap>
+              <Spacer />
+              <Link href={bookUrl} passHref>
+                <Button
+                  as="a"
+                  colorScheme="messenger"
+                  rounded="full"
+                  variant="outline"
+                  rightIcon={<Icon as={IoArrowForward} />}
+                >View book</Button>
+              </Link>
+            </HStack>
+          </Box>
         </Box>
       </ModalContent>
     </Modal>
